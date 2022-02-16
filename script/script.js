1ff424385e99c906d6cc06bb60ed4a96b63e2a33
Kamil Salakhov <antdead19@gmail.com>
2022-02-16 19:09:22 +0300

    feat: change enter name

--- script/script.js
+++ script/script.js
@@ -1,8 +1,10 @@
-var canvas = document.getElementById('game');
+const canvas = document.querySelector('.game');
 var context = canvas.getContext('2d');
-var grid = 16;
-var count = 0;
-var snake = {
+const textName = document.querySelector('.text_name');
+const submit = document.querySelector('.submit');
+const grid = 16;
+let count = 0;
+const snake = {
     x: 160,
     y: 160,
     dx: grid,
@@ -11,47 +13,69 @@ var snake = {
     maxCells: 4,
     level: 0
 };
-var apple = {
+const apple = {
     x: 320,
     y: 320
 };
+let isPlay = false;
 
 // количество набранных очков на старте 
 let score = 0;
 // рекорд игры 
 let record = 0;
 // текущий уровень сложности 
-let level = 1; 
+let level = 1;
 // имя игрока с наибольшим рейтингом 
+let gamerName = '';
 let recordName = '';
-name = prompt("Ваше имя", "");
 //скорость движения змеи
 let speed = 6;
 
+
+// Функция ввода имени и запуска игры
+function nameLoad() {
+    localStorage.clear()
+    gamerName = textName.value;
+
+    localStorage.setItem('gamerName', `${textName.value}`);
+}
+
+submit.addEventListener('click', nameLoad)
+
 // Узнаём размер хранилища 
-//localStorage.clear()
-let Storage_size = localStorage.length; 
+//
+let Storage_size = localStorage.length;
 // Если в хранилище уже что-то есть… 
-if (Storage_size > 0) { 
+if (Storage_size > 0) {
     // …то достаём оттуда значение рекорда и имя чемпиона 
-    record = localStorage.record; 
-    recordName = localStorage.recordName;
-};
+    record = localStorage.getItem('record');
+    recordName = localStorage.getItem('recordName');
+} else {
+
+
+}
+
+
+
 
 // получаем доступ к холсту с игровой статистикой
-const canvasScore = document.getElementById('score'); 
+const canvasScore = document.querySelector('.score');
 const contextScore = canvasScore.getContext('2d');
 
 //Делаем генератор случайных чисел в заданном диапазоне.
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;    
+    return Math.floor(Math.random() * (max - min)) + min;
 };
+
+
+
 function showScore() {
-    contextScore.clearRect(0,0,canvasScore.width,canvasScore.height);  
+    contextScore.clearRect(0, 0, canvasScore.width, canvasScore.height);
     contextScore.globalAlpha = 1;
     contextScore.fillStyle = 'orange';
     contextScore.font = '18px Courier New';
-    contextScore.fillText('SNAKE', 120, 80);
+    contextScore.fillText('SNAKE', 120, 60);
+    contextScore.fillText('Игрок:   ' + gamerName, 15, 100);
     contextScore.fillText('Уровень: ' + level, 15, 150);
     contextScore.fillText('Очков:   ' + score, 15, 250);
     contextScore.fillText('Чемпион: ' + recordName, 160, 150);
@@ -60,11 +84,12 @@ function showScore() {
 
 //Игровой цикл - основной процесс, внутри которого будет все происходить
 function loop() {
+
     // Дальше будет хитрая функция, которая замедляет скорость игры с 60 кадров в секунду до 15. Для этого она пропускает три кадра из четырёх, то есть срабатывает каждый четвёртый кадр игры. Было 60 кадров в секунду, станет 15.
-    requestAnimationFrame (loop);
+    window.requestAnimationFrame(loop);
     // Игровой код выполнится только один раз из четырёх, в этом и суть замедления кадров, а пока переменная count меньше четырёх, код выполняться не будет. 
     if (++count < speed) {
-        return;        
+        return;
     }
     //Обнуляем переменную скорости
     count = 0;
@@ -75,23 +100,21 @@ function loop() {
     snake.y += snake.dy;
     // Если змейка достигла края поля по горизонтали — продолжаем её движение с противоположной стороны
     if (snake.x < 0) {
-        snake.x = canvas.width - grid;        
-    }
-    else if (snake.x >= canvas.width) {
-        snake.x = 0;        
+        snake.x = canvas.width - grid;
+    } else if (snake.x >= canvas.width) {
+        snake.x = 0;
     }
     // Делаем тоже самое для движения по вертикали
     if (snake.y < 0) {
-        snake.y = canvas.height - grid;        
+        snake.y = canvas.height - grid;
+    } else if (snake.y >= canvas.height) {
+        snake.y = 0;
     }
-    else if (snake.y >= canvas.height) {
-        snake.y = 0;        
-    } 
     // Продолжаем двигаться в выбранном направлении. Голова всегда впереди, поэтому добавляем её координаты в начало массива, который отвечает за всю змейку.
-    snake.cells.unshift({x: snake.x, y: snake.y});
+    snake.cells.unshift({ x: snake.x, y: snake.y });
     // Сразу после этого удаляем последний элемент из массива змейки, потому что она движется и постоянно особождает клетки после себя
     if (snake.cells.length > snake.maxCells) {
-        snake.cells.pop();        
+        snake.cells.pop();
     }
     // Рисуем еду — красное яблоко
     context.fillStyle = 'red';
@@ -99,7 +122,7 @@ function loop() {
     // Одно движение змейки — один новый нарисованный квадратик
     context.fillStyle = 'green';
     // Обрабатываем каждый элемент змейки
-    snake.cells.forEach(function (cell, index) {
+    snake.cells.forEach(function(cell, index) {
         // Чтобы создать эффект клеточек, делаем зелёные квадратики меньше на один пиксель, чтобы вокруг них образовалась чёрная граница
         context.fillRect(cell.x, cell.y, grid - 1, grid - 1);
         // Если змейка добралась до яблока...
@@ -109,20 +132,20 @@ function loop() {
             //увеличивем счетчик
             score++;
             //увеличиваем скорость при набранных очках
-            if (score == 5 || score == 10 || score == 15 || score == 40 ) {
-                speed --;
-                level ++;
+            if (score == 5 || score == 10 || score == 15 || score == 40) {
+                speed--;
+                level++;
             };
-           
+
             // Рисуем новое яблочко
             // Помним, что размер холста у нас 400x400, при этом он разбит на ячейки — 25 в каждую сторону
-            apple.x = getRandomInt (0, 25) * grid;
-            apple.y = getRandomInt (0, 25) * grid;
+            apple.x = getRandomInt(0, 25) * grid;
+            apple.y = getRandomInt(0, 25) * grid;
         }
-        
+
         // Проверяем, не столкнулась ли змея сама с собой
         // Для этого перебираем весь массив и смотрим, есть ли у нас в массиве змейки две клетки с одинаковыми координатами
-        for (var i = index + 1; i < snake.cells.length; i++) {
+        for (let i = index + 1; i < snake.cells.length; i++) {
             // Если такие клетки есть — начинаем игру заново
             if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
                 // Задаём стартовые параметры основным переменным
@@ -135,17 +158,17 @@ function loop() {
                 // Ставим яблочко в случайное место
                 apple.x = getRandomInt(0, 25) * grid;
                 apple.y = getRandomInt(0, 25) * grid;
-                
+
                 // если игрок побил прошлый рекорд           
                 if (score > record) {
                     // ставим его очки как рекорд
                     record = score;
                     // заносим в хранилище значение рекорда 
-                    localStorage.record = record;
+                    localStorage.setItem('record', `${record}`);
                     // меняем имя чемпиона 
-                    recordName = name;
+                    recordName = gamerName;
                     // заносим в хранилище его имя 
-                    localStorage.recordName = recordName;
+                    localStorage.setItem('recordName', `${gamerName}`);
                 };
                 score = 0;
                 level = 1;
@@ -153,36 +176,37 @@ function loop() {
             }
         }
     });
-// выводим статистику 
-showScore();
+    // выводим статистику 
+    showScore();
 }
 
 // Смотрим, какие нажимаются клавиши, и реагируем на них нужным образом
-document.addEventListener('keydown', function (e) {
+document.addEventListener('keydown', function(event) {
     // Дополнительно проверяем такой момент: если змейка движется, например, влево, то ещё одно нажатие влево или вправо ничего не поменяет — змейка продолжит двигаться в ту же сторону, что и раньше. Это сделано для того, чтобы не разворачивать весь массив со змейкой на лету и не усложнять код игры. 
     // Стрелка влево
     // Если нажата стрелка влево, и при этом змейка никуда не движется по горизонтали…
-    if (e.which === 37 && snake.dx === 0) {
+    if (event.key === 'ArrowLeft' && snake.dx === 0) {
         // то даём ей движение по горизонтали, влево, а вертикальное — останавливаем
         // Та же самая логика будет и в остальных кнопках
-        snake.dx = - grid;
+        snake.dx = -grid;
         snake.dy = 0;
     }
     // Стрелка вверх
-    else if (e.which === 38 && snake.dy === 0) {
+    else if (event.key === 'ArrowUp' && snake.dy === 0) {
         snake.dy = -grid;
-        snake.dx = 0;        
+        snake.dx = 0;
     }
     // Стрелка вправо
-    else if (e.which === 39 && snake.dx === 0) {
+    else if (event.key === 'ArrowRight' && snake.dx === 0) {
         snake.dx = grid;
-        snake.dy = 0; 
+        snake.dy = 0;
     }
     // Стрелка вниз
-    else if (e.which === 40 && snake.dy === 0) {
+    else if (event.key === 'ArrowDown' && snake.dy === 0) {
         snake.dy = grid;
-        snake.dx = 0;        
+        snake.dx = 0;
     }
 });
+
 // Запускаем игру
-requestAnimationFrame(loop);
+document.querySelector('.play-button').addEventListener('click', loop)
